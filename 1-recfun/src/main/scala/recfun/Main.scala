5d4f2eaddc6f4ea73df9a4fdb7bf00f8b3cbe46e
Cristina Delgado <cris3wdev@gmail.com>
2016-07-08 18:35:29 +0200

    Add solution of assignment 1-recfun

--- 1-recfun/src/main/scala/recfun/Main.scala
+++ 1-recfun/src/main/scala/recfun/Main.scala
@@ -1,6 +1,10 @@
 package recfun
 
+import scala.annotation.tailrec
+
+
 object Main {
+
   def main(args: Array[String]) {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
@@ -13,15 +17,41 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
-  
+  def pascal(c: Int, r: Int): Int = {
+    if  (c == 0 || c == r || r == 0)  1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
+
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
-  
+  def balance(chars: List[Char]): Boolean = {
+
+    @tailrec
+    def balanceIter(chars: List[Char], acc: Int): Boolean = {
+      if (chars.isEmpty) acc == 0
+      else if (chars.head == '(') balanceIter(chars.tail, acc + 1)
+      else if (chars.head == ')' && acc > 0) balanceIter(chars.tail, acc - 1)
+      else if (chars.head == ')' && acc == 0) false
+      else balanceIter(chars.tail, acc)
+    }
+
+    balanceIter(chars, 0)
+  }
+
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+
+    def countChangeIter(money: Int, coins: List[Int], acc: Int): Int = {
+      if (money < 0) acc
+      else if (coins.isEmpty)
+        if (money == 0) acc + 1 else acc
+      else countChangeIter(money - coins.head, coins, acc) + countChangeIter(money, coins.tail, acc)
+    }
+
+    countChangeIter(money, coins, 0)
   }
+
+}
